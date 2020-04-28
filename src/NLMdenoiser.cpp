03b1d1dfcdb9d3c5f791c6872540ffed3575f356
Dino Malpera <dino.malpera@gmail.com>
2020-04-27 20:00:03 -0700

    Moved temporary scheduler into a separate header.

--- src/NLMdenoiser.cpp
+++ src/NLMdenoiser.cpp
@@ -10,6 +10,8 @@
 #include <algorithm>
 #include <vector>
 
+#include "Naive_Work_Scheduler.h"
+
 using namespace VNLM;
 
 static
@@ -151,39 +153,6 @@ join_vector_of_threads(
             thr.join();
         } );
 }
-
-class Naive_Work_Scheduler
-{
-public:
-    Naive_Work_Scheduler(
-            const FrameSize&    _frameSize,
-            const unsigned int  _numThreads
-    )
-        :   frameSize       (_frameSize)
-        ,   rows_per_thread (_frameSize.size_y / _numThreads)
-        ,   last_row        (_numThreads - 1U)
-    {
-    }
-public:
-    auto
-    get_range(
-            const unsigned int thr ) const
-    {
-        const   Every_Pixel_In_a_Frame fpr(frameSize);
-                Pixel_Range_Iterator b =    fpr.begin() + ((rows_per_thread * thr)*frameSize.size_x);
-        const   Pixel_Range_Iterator e =    (thr == last_row)
-                                            ?
-                                            fpr.end()
-                                            :
-                                            (fpr.begin() + ((rows_per_thread * (thr+1))*frameSize.size_x));
-        
-        return std::tuple( b, e );
-    }
-private:
-    const FrameSize&    frameSize;
-    const unsigned int  rows_per_thread;
-    const unsigned int  last_row;
-};
 
 template <typename Pixel_Value_policy>
 void
---
+++ src/Naive_Work_Scheduler.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "FrameSize.h"
+
+namespace VNLM
+{
+
+	class Naive_Work_Scheduler
+	{
+	public:
+	    Naive_Work_Scheduler(
+		    const FrameSize&    _frameSize,
+		    const unsigned int  _numThreads
+	    )
+		:   frameSize       (_frameSize)
+		,   rows_per_thread (_frameSize.size_y / _numThreads)
+		,   last_row        (_numThreads - 1U)
+	    {
+	    }
+	public:
+	    auto
+	    get_range(
+		    const unsigned int thr ) const
+	    {
+		const   Every_Pixel_In_a_Frame fpr(frameSize);
+		        Pixel_Range_Iterator b =    fpr.begin() + ((rows_per_thread * thr)*frameSize.size_x);
+		const   Pixel_Range_Iterator e =    (thr == last_row)
+		                                    ?
+		                                    fpr.end()
+		                                    :
+		                                    (fpr.begin() + ((rows_per_thread * (thr+1))*frameSize.size_x));
+		
+		return std::tuple( b, e );
+	    }
+	private:
+	    const FrameSize&    frameSize;
+	    const unsigned int  rows_per_thread;
+	    const unsigned int  last_row;
+	};
+
+}
+
